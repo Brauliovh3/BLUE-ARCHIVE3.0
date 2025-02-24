8986b85b6980cc3f4e87870d15196d14ac4e1415
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2025-02-24 15:59:00 -0600

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,24 +1,42 @@
-import {WAMessageStubType} from '@whiskeysockets/baileys'
+import { WAMessageStubType } from '@whiskeysockets/baileys'
 import fetch from 'node-fetch'
 
-export async function before(m, {conn, participants, groupMetadata}) {
+export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return !0;
   let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://tinyurl.com/2cd94clt')
   let img = await (await fetch(`${pp}`)).buffer()
   let chat = global.db.data.chats[m.chat]
 
   if (chat.bienvenida && m.messageStubType == 27) {
-    let bienvenida = `▛01 *${botname}* ▟\n▍ ▛ Bienvenido ▟ \n▙01 「 @${m.messageStubParameters[0].split`@`[0]} ▟ \n   ▍01  Bienvenido a\n   █01  ${groupMetadata.subject}\n   ▙▰▰▰▰▰▰▰▰▰▰▰01`
+    let bienvenida = `💙━━⊱【Hatsune Miku】⊰━━💙
+🌟 ¡Bienvenid@! 🌟
+🎤 Usuario: @${m.messageStubParameters[0].split`@`[0]}
+🎶 Grupo: ${groupMetadata.subject}
+💙 ¡Diviértete y disfruta la música! 🎵
+💙━━⊱【01】⊰━━💙`
     
-await conn.sendAi(m.chat, botname, textbot, bienvenida, img, img, canal, estilo)
+    await conn.sendAi(m.chat, botname, textbot, bienvenida, img, img, canal, estilo)
   }
-  
+
   if (chat.bienvenida && m.messageStubType == 28) {
-    let bye = `▛01 *${botname}* ▟\n▍ ▛ ADIOS 👋 ▟ \n▙01 ▛ @${m.messageStubParameters[0].split`@`[0]} ▟ \n   ▍01  Se fue\n   ▍01 Jamás te quisimos aquí\n   ▙▰▰▰▰▰▰▰▰▰▰01`
-await conn.sendAi(m.chat, botname, textbot, bye, img, img, canal, estilo)
+    let bye = `💙━━⊱【Hatsune Miku】⊰━━💙
+🚫 Expulsión confirmada.  
+🎤 Usuario: @${m.messageStubParameters[0].split`@`[0]}
+💔 Fue removido del grupo.
+💙 Que el ritmo siga sin interrupciones. 🎵
+💙━━⊱【01】⊰━━💙`
+
+    await conn.sendAi(m.chat, botname, textbot, bye, img, img, canal, estilo)
   }
-  
+
   if (chat.bienvenida && m.messageStubType == 32) {
-    let kick = `▛01 *${botname}* ▟\n▍ ▛ ADIOS 👋 ▟\n▙01 ▛ @${m.messageStubParameters[0].split`@`[0]} ▟ \n   ▍01  Se fue\n   ▍01 Jamás te quisimos aquí\n   ▙▰▰▰▰▰▰▰▰▰01`
-await conn.sendAi(m.chat, botname, textbot, kick, img, img, canal, estilo)
-}}
+    let kick = `💙━━⊱【Hatsune Miku】⊰━━💙
+😢 Adiós, usuario.  
+🎤 Usuario: @${m.messageStubParameters[0].split`@`[0]}
+💔 Se fue del grupo...
+💙 Nunca olvidaremos tu eco. 🎶
+💙━━⊱【01】⊰━━💙`
+
+    await conn.sendAi(m.chat, botname, textbot, kick, img, img, canal, estilo)
+  }
+}
