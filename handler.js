89cd0261f5c075432cd9b90791ba3afe76745665
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2025-03-04 10:26:28 -0600

    Update handler.js

--- handler.js
+++ handler.js
@@ -89,6 +89,8 @@ export async function handler(chatUpdate) {
                     chat.bienvenida = true 
                 if (!('antiLink' in chat))
                     chat.antiLink = false
+                if (!('antilink2' in chat))
+                    chat.antilink2 = false;
                 if (!('onlyLatinos' in chat))
                     chat.onlyLatinos = false
                  if (!('nsfw' in chat))
