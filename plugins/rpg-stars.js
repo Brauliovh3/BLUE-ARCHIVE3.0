6f4261857b9f6a2b00e072ff784763f36d055615
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2025-03-29 12:10:10 -0600

    Update rpg-stars.js

--- plugins/rpg-stars.js
+++ plugins/rpg-stars.js
@@ -7,16 +7,16 @@ let handler = async (m, {conn, usedPrefix}) => {
     let name = conn.getName(who)
     if (!(who in global.db.data.users)) return conn.reply(m.chat, '💙 El usuario no se encuentra en mi base de Datos.', m, rcanal).then(_ => m.react('✖️'))
     let img = await (await fetch(`https://w7.pngwing.com/pngs/531/1011/png-transparent-hatsune-miku-vocaloid-chibi-holography-anime-hatsune-miku-fictional-characters-manga-chibi.png`)).buffer()
-    let txt = ` –  *S T A R S  -  U S E R*\n\n`
+    let txt = ` –  *C E B O L L I N E S  -  U S E R*\n\n`
         txt += `┌  💙  *Nombre* : ${user.name}\n`
-        txt += `│  💙  *Estrellas* : ${toNum(user.limit)} ( *${user.limit}* )\n`
+        txt += `│  💙  *Cebollines* : ${toNum(user.limit)} ( *${user.limit}* )\n`
         txt += `│  💙  *Bank* : ${toNum(user.bank)} ( *${user.bank}* )\n`
         txt += `└  💙  *XP* : ${toNum(user.exp)} ( *${user.exp}* )`
     let mentionedJid = [who]
         
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
 }
-handler.help = ['estrellas']
+handler.help = ['Cebollines']
 handler.tags = ['rpg']
 handler.command = ['coins', 'wallet', 'cartera', 'cebollines', 'cebollin', 'bal', 'balance']
 handler.register = true 
