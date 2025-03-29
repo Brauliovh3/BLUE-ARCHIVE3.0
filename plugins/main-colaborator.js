f39e708fcdb4666a4b94f8240b74720bf89f8eec
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2025-03-29 10:59:41 -0600

    Delete plugins directory

--- plugins/main-colaborator.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Rafael;;\nFN:Rafael\nORG:Rafael⁩\nTITLE:\nitem1.TEL;waid=51939508653:51939508653\nitem1.X-ABLabel:Rafael⁩\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Rafael\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'Rafael⁩', contacts: [{ vcard }] }}, {quoted: m})
-}
-handler.help = ['colaborator']
-handler.tags = ['main']
-handler.command = ['colab', 'colaborator', 'colaborador', 'ayudante'] 
-
-export default handler
