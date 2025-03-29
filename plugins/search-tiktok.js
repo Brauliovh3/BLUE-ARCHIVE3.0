2505daff6a5fa71a2dfc07a11f02552ebf053eaa
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2025-03-29 12:08:11 -0500

    Add files via upload

---
+++ plugins/search-tiktok.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+import Starlights from '@StarlightsTeam/Scraper'
+
+let handler = async (m, { conn, usedPrefix, command, text, args }) => {
+  if (!text) return conn.reply(m.chat, `💙  Ingresa el nombre video que deseas buscar en TikTok.\n\nEjemplo:\n> *${usedPrefix + command}* Hatsune Miku Edit`, m, rcanal)
+  
+  await m.react('🕓')
+  let img = await (await axios.get('https://i.ibb.co/S0QrZsy/file.jpg', { responseType: 'arraybuffer' })).data
+
+  try {
+    let data = await Starlights.tiktokSearch(text)
+
+    if (data && data.length > 0) {
+      let txt = `*💙  T I K T O K  -  S E A R C H  💙*`
+      for (let i = 0; i < (50 <= data.length ? 50 : data.length); i++) {
+        let video = data[i]
+        txt += `\n\n`
+        txt += `  *💙 Nro* : ${i + 1}\n`
+        txt += `  *💙 Título* : ${video.title}\n`
+        txt += `  *💙 Autor* : ${video.author}\n`
+        txt += `  *💙 Url* : ${video.url}`
+      }
+      await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
+      await m.react('✅')
+    } else {
+      await conn.react('✖️')
+    }
+  } catch {
+    await m.react('✖️')
+  }
+}
+handler.tags = ['search']
+handler.help = ['tiktoksearch *<búsqueda>*']
+handler.command = ['tiktoksearch', 'tiktoks']
+handler.register = true
+
+export default handler
