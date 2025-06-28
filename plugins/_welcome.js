30426cb7937f370ff4d1661ee143b3d1d23a2420
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2025-06-28 12:18:40 -0500

    💚BLUE ARCHIVE💚

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -3,22 +3,22 @@ import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://tinyurl.com/2cd94clt')
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://media.tenor.com/if2-iI3keS0AAAAe/tachibana-nozomi-nozomi.png')
   let img = await (await fetch(`${pp}`)).buffer()
   let chat = global.db.data.chats[m.chat]
 
   if (chat.bienvenida && m.messageStubType == 27) {
-    let bienvenida = `💙━━⊱ *${botname}* ⊰━━💙 \n🌟 *¡Bienvenid@!* 🌟\n🎤 *Usuario:@${m.messageStubParameters[0].split`@`[0]} \n  🎶 *Grupo: ${groupMetadata.subject}\n💙 *¡Que la música te acompañe!* 🎵 \n   💙━━⊱【01】⊰━━💙`
+    let bienvenida = `💚━━⊱ *${botname}* ⊰━━💚 \n🌟 *¡Bienvenid@!* 🌟\n🎤 *Usuario:@${m.messageStubParameters[0].split`@`[0]} \n  🎶 *Grupo: ${groupMetadata.subject}\n💚 *¡Que la compañia te ayude!* 🎵 \n   💚━━⊱【🖨】⊰━━💚`
     
 await conn.sendAi(m.chat, botname, textbot, bienvenida, img, img, canal, estilo)
   }
   
   if (chat.bienvenida && m.messageStubType == 28) {
-    let bye = `💙━━⊱ *${botname}* ⊰━━💙 \n🚫 *Expulsión confirmada.\n🎤 *Usuario: @${m.messageStubParameters[0].split`@`[0]} \n  💔 *Fue removido del grupo.\n   💙 *Que el ritmo siga sin interrupciones.\n   💙━━⊱【01】⊰━━💙`
+    let bye = `💚━━⊱ *${botname}* ⊰━━💚 \n🚫 *Expulsión confirmada.\n🎤 *Usuario: @${m.messageStubParameters[0].split`@`[0]} \n  💔 *Fue removido del grupo.\n   💚 *Que el tren siga sin interrupciones.\n   💚━━⊱【🖨】⊰━━💚`
 await conn.sendAi(m.chat, botname, textbot, bye, img, img, canal, estilo)
   }
   
   if (chat.bienvenida && m.messageStubType == 32) {
-    let kick = `💙━━⊱ *${botname}* ⊰━━💙 \n😢 *Adiós, usuario.*\n🎤 *Usuario:「 @${m.messageStubParameters[0].split`@`[0]} \n   💔 *Se fue del grupo...\n   💙 *Nunca olvidaremos tu eco.* 🎶\n   💙━━⊱【01】⊰━━💙`
+    let kick = `💚━━⊱ *${botname}* ⊰━━💚 \n😢 *Adiós, usuario.*\n🎤 *Usuario:「 @${m.messageStubParameters[0].split`@`[0]} \n   💔 *Se fue del grupo...\n   💚 *Nunca te olvidaremos estudiante.* 🎶\n   💚━━⊱【🖨】⊰━━💚`
 await conn.sendAi(m.chat, botname, textbot, kick, img, img, canal, estilo)
 }}
