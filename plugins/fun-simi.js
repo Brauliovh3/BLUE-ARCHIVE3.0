da2ce6be98a85c1450d868dd95a54665f6d493e2
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2025-06-29 17:50:41 -0500

    💚BLUE ARCHIVE💚

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -1,7 +1,7 @@
 import translate from '@vitalets/google-translate-api'
 import fetch from 'node-fetch'
 const handler = async (m, {text, command, args, usedPrefix}) => {
-  if (!text) return m.reply(`*• Ingresa un texto*\n\n*Ejemplo:*\n*${usedPrefix + command}* Hola bot`)
+  if (!text) return m.reply(`*💚 Ingresa un texto*\n\n*Ejemplo:*\n*${usedPrefix + command}* SENSEI DEPOOL`)
   try {
     const api = await fetch('https://api.simsimi.net/v2/?text=' + text + '&lc=es');
     const resSimi = await api.json();
@@ -20,7 +20,7 @@ const handler = async (m, {text, command, args, usedPrefix}) => {
       const resu2 = await reis2.json();
       m.reply(resu2[0][0][0]);
     } catch {
-      throw `*Miku Bot😺* | 「 *ERROR* 」\n\nOcurrió un *Error*`;
+      throw `*💚 nozomi error* | 「 *ERROR* 」\n\nOcurrió un *Error*`;
     }
   }
 };
