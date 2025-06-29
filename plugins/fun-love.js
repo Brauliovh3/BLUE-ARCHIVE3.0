eedae81e947895c6e5cb12c1a482f8b3f1a9cf4a
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2025-06-29 17:45:17 -0500

    💚BLUE ARCHIVE💚

--- plugins/fun-love.js
+++ plugins/fun-love.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, command, text }) => {
-let love = `*💙💙 MEDIDOR DE AMOR 💙💙*
+let love = `*💚💚 MEDIDOR DE AMOR 💚💚*
 *El amor de ${text} por ti es de* *${Math.floor(Math.random() * 100)}%* *de un 100%*
-*Deberias pedirle que sea tu  novia/o ?*
+*💚 Deberias pedirle que sea tu  novia/o ?*
 `.trim()
 m.reply(love, null, { mentions: conn.parseMention(love) })}
 handler.help = ['love *@user*']
