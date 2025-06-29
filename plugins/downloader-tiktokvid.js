96dedac788509e8f76a2f371bc987e6d93253f28
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2025-06-28 20:00:36 -0500

    💚BLUE ARCHIVE💚

--- plugins/downloader-tiktokvid.js
+++ plugins/downloader-tiktokvid.js
@@ -1,22 +1,22 @@
 import Starlights from '@StarlightsTeam/Scraper'
 
 let handler = async (m, { conn, usedPrefix, command, text, args }) => {
-  if (!text) return conn.reply(m.chat, '💙 Ingresa un texto junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Hatsune Miku Senbonzakura`, m, rcanal)
+  if (!text) return conn.reply(m.chat, '💚 Ingresa un texto junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Blue Archive Hakari`, m, rcanal)
   await m.react('🕓')
   try {
     let { title, author, duration, views, likes, comments_count, share_count, download_count, published, dl_url } = await Starlights.tiktokvid(text)
 
-      let txt = '`💙  T I K T O K  -  D O W N L O A D  💙`\n\n'
-          txt += `    💙  *Título* : ${title}\n`
-          txt += `    💙  *Autor* : ${author}\n`
-          txt += `    💙  *Duración* : ${duration} segundos\n`
-          txt += `    💙  *Vistas* : ${views}\n`
-          txt += `    💙  *Likes* : ${likes}\n`
-          txt += `    💙  *Comentarios* : ${comments_count}\n`
-          txt += `    💙  *Compartidos* : ${share_count}\n`
-          txt += `    💙  *Publicado* : ${published}\n`
-          txt += `    💙  *Descargas* : ${download_count}\n\n`
-          txt += `> 🌱 ${textbot}`
+      let txt = '`💚  T I K T O K  -  D O W N L O A D  💚`\n\n'
+          txt += `    💚  *Título* : ${title}\n`
+          txt += `    💚  *Autor* : ${author}\n`
+          txt += `    💚  *Duración* : ${duration} segundos\n`
+          txt += `    💚  *Vistas* : ${views}\n`
+          txt += `    💚  *Likes* : ${likes}\n`
+          txt += `    💚  *Comentarios* : ${comments_count}\n`
+          txt += `    💚  *Compartidos* : ${share_count}\n`
+          txt += `    💚  *Publicado* : ${published}\n`
+          txt += `    💚  *Descargas* : ${download_count}\n\n`
+          txt += `> 🚂 ${textbot}`
 
       await conn.sendFile(m.chat, dl_url, `thumbnail.mp4`, txt, m)
       await m.react('✅')
