0b632762304fb34c8c3376cb162b7af1172d5021
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2025-07-11 15:56:05 -0500

    💚BLUE ARCHIVE💚

--- plugins/downloader-spotifydl.js
+++ plugins/downloader-spotifydl.js
@@ -1,36 +1,36 @@
-import Starlights from '@StarlightsTeam/Scraper';
-import fetch from 'node-fetch';
+import fetch from 'node-fetch'
 
-let handler = async (m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, '💚 Ingresa el enlace de algún Track, Playlist o Álbum de Spotify.', m);
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    if (!text) { return conn.reply(m.chat, '💚 Ingresa el enlace de algún Track de Spotify.\n\n' + '`Ejemplo:`\n' + `> *${usedPrefix + command}* https://open.spotify.com/track/5hnOJqBnVXRlc7JS49IChB`, m, rcanal)}
 
-let isSpotifyUrl = text.match(/^(https:\/\/open\.spotify\.com\/(album|track|playlist)\/[a-zA-Z0-9]+)/i);
-if (!isSpotifyUrl) return conn.reply(m.chat, '💚 Ingresa un enlace válido de Track, Playlist o Álbum de Spotify.', m);
+  let isSpotifyTrack = text.match(/^https:\/\/open\.spotify\.com\/track\/[a-zA-Z0-9]+/i)
+  if (!isSpotifyTrack) return conn.reply(m.chat, '💚 Solo se permiten enlaces de *Tracks* de Spotify.', m, rcanal)
 
-await m.react('🕓')
-try {
-let { title, artist, album, thumbnail, dl_url } = await Starlights.spotifydl(text);
-let img = await (await fetch(thumbnail)).buffer();
+  await m.react('🕓')
+  try {
+    let res = await fetch(`https://api.starlights.uk/api/downloader/spotify?url=${text}`)
+    let json = await res.json()
 
-let txt = `*💚  S P O T I F Y  -  D O W N L O A D  💚*\n\n`;
-    txt += `    💚  *Título* : ${title}\n`;
-    txt += `    💚  *Álbum* : ${album}\n`;
-    txt += `    💚  *Artista* : ${artist}\n\n`;
-    txt += `*- 🚂 Los audios se están enviando, espera un momento. . .*`;
+    let { title, artist, album, thumbnail, download } = json.result
 
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal);
-await conn.sendMessage(m.chat, { audio: { url: dl_url }, fileName: `${title}.mp3`, mimetype: 'audio/mp4' }, { quoted: m });
+    let txt = '*💚 SPOTIFY 💚⁭ DL`*\n\n'
+        txt += `\t*💚 Titulo* :: ${title}\n`
+        txt += `\t*💚 Artista* :: ${artist}\n`
+        txt += `\t*💚 Album* :: ${album || 'Desconocido'}\n\n`
+        txt += `> *- 🚂 El audio se está enviando, espera un momento...*`
 
-await m.react('✅');
-} catch {
-await m.react('✖️');
+    await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', txt, m, null, rcanal)
+    await conn.sendMessage(m.chat, { audio: { url: download }, fileName: `${title}.mp3`, mimetype: 'audio/mp4' }, { quoted: m })
+
+    await m.react('✅')
+  } catch {
+    await m.react('✖️')
+  }
 }
-};
 
-handler.help = ['spotifydl'];
-handler.tags = ['downloader'];
-handler.command = ['spotifydl'];
-// handler.limit = 1;
-handler.register = true;
+handler.help = ['spotifydl']
+handler.tags = ['downloader']
+handler.command = ['spotifydl']
+handler.register = true
 
-export default handler;
+export default handler
